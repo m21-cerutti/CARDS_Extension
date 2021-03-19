7799ca104becf67f857354e546ce8500dda955ad
Lorrain <gabrielorrain7@gmail.com>
2021-03-19 13:02:34 +0100

    fix merge

--- cards_rgbtrack/include/tracker_color.h
+++ cards_rgbtrack/include/tracker_color.h
@@ -17,10 +17,6 @@ public:
 
 private:
 
-	short findColor( Mat img );
-
-private:
-
 	Mat bgr_frame;
 	Mat hsv_frame;
 	Mat thresh_frame;
@@ -29,16 +25,7 @@ private:
 	//TODO #define
 	short epsilon = 10;
 	int minArea = 10;
-<<<<<<< HEAD
 
 	short findColor( Mat img );
 	int maxArea(vector<vector<Point>> contours);
-
-public:
-	TrackerCOLOR();
-	bool init( InputArray image,const Rect2d& boundingBox ) override;
-	bool update( InputArray image,Rect2d& boundingBox ) override;
-	static Ptr<ITracker> create();
-=======
->>>>>>> devellop
 };
